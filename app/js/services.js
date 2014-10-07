6ab47e5a519784c4e7310486950d5d0a4f2dc87c
Lukasz <korbolewski@gmail.com>
2014-10-07 15:14:03 +0200

    further changes

--- app/js/services.js
+++ app/js/services.js
@@ -4,13 +4,26 @@ angular.module('F1FeederApp.services', []).
 		var ergastAPI = {};
 
 		ergastAPI.getDrivers = function() {
-			// body...
 			return $http({
 				method: 'JSONP',
 				url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
 			});
 		}
 		
+		ergastAPI.getDriverDetails = function(id) {
+			return $http({
+				method: "JSONP",
+				url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+			})
+		}
+
+		ergastAPI.getDriverRaces = function(id) {
+			return $http({
+				method: "JSONP",
+				url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+			})
+		}
+
 		return ergastAPI;
 
 	});
